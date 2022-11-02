d947370448ac46a9241ac31d0e0ab9d6ee1d1d4b
sahba atarodi <sahba.atarodi@gmail.com>
2022-11-01 20:59:09 -0400

    the program is done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Mooki";
+            int dogAge = 1;
+            char firstInitial = 'M';
+            bool isHungry = true;
+            double dogWeight = 12.5;
+            decimal dogHeight = 9.7m;
+
+
+            Console.WriteLine($"My dog's name is {dogName} and her initial is {firstInitial}. " +
+                $" SHe is {dogAge} years old. SHe weighs {dogWeight} pounds," +
+                $" and She is {dogHeight} inches long");
+            Console.WriteLine($"It is {isHungry} that Mooki is always hungry.");
+
         }
     }
 }
